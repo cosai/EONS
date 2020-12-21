ec16185a23c635dfb6451a5db3850f85224d7ac4
cosai <cosai@users.noreply.github.com>
2020-12-21 14:26:48 -0500

    Update compile.bat
    jar file generation added

--- compile.bat
+++ compile.bat
@@ -1,3 +1,7 @@
 @echo off
+echo compiling the project
 javac -d ./bin/ -cp ./bin/ ./src/simulator/*.java
+echo.
+echo Creating jar file
+jar cvfe Simulator.jar bin.simulator.Simulator ./bin/simulator/*.class
 pause
