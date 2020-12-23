266242e409ea51ff8709209ba7bb8621c0eb4cf7
cosai <cosai@users.noreply.github.com>
2020-12-22 23:17:27 -0500

    Delete compile.bat
    wrong file deleted

--- compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo compiling the project
-javac -d ./bin/ -cp ./bin/ ./src/simulator/*.java
-echo.
-echo Creating jar file
-jar cvfe Simulator.jar bin.simulator.Simulator ./bin/simulator/*.class
-pause
